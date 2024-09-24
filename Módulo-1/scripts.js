12bcae335acd20e7bf6a89a66bf833ff1ea88e4a
VitorVGDev <vgvital@edu.unifil.br>
2024-09-24 11:17:32 -0300

    primeiro-commit

---
+++ Módulo-1/scripts.js
@@ -0,0 +1,12 @@
+/*Funções de impressão*/ 
+var nome = "Vitor";
+alert("Olá, " + nome + "! Você está aprendendo JavaScript");
+console.log("Esta mensagem aparece no console.");
+
+
+let idade = "18";
+const cidade = "Londrina";
+var positivo = true;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
